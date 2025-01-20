bb8e4e8326f83c38250ac9002faa005351c07b7f
Stanislav <stanislav.kuperstein@gmail.com>
2025-01-20 15:46:40 +0100

    Fixed the categories_dict and the xgboost_model  size by removing 'trans_num' from the training set

--- airflow/dags/my_dag.py
+++ airflow/dags/my_dag.py
@@ -26,7 +26,7 @@ def predict_fraud(api_request_dict):
 
         # Remove 'trans_num' from the columns as it's not used in the model
         df.drop(columns=['trans_num'], inplace=True)
-        
+
         file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir, 'modelling/category_dict.pkl')
         # Load the categorical dictionary
         with open(file_path, 'rb') as file:
@@ -87,6 +87,122 @@ def make_api_request():
         # Log any errors encountered during the prediction               
         logging.error(f"Error during API request: {str(e)}")
         return None  # Return None if an exception occurs
+
+
+def create_database_connection():
+    try:
+        # Load environment variables from the .env file
+        load_dotenv()
+
+        # Access the variables
+        logging.info("Loading credentials ...")
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+        DB_NAME = os.getenv('DB_NAME')
+        DB_USER = os.getenv('DB_USER')
+        DB_PASS = os.getenv('DB_PASS')
+        INSTANCE_CONNECTION_NAME = os.getenv('INSTANCE_CONNECTION_NAME')
+
+        # Initialize Cloud SQL Connector
+        logging.info("Initializing Cloud SQL connector...")  
+        connector = Connector()
+        
+        # Create a connection to the database using pg8000 driver
+        logging.info("Connecting to the database...")
+        connection = connector.connect(
+            INSTANCE_CONNECTION_NAME,
+            "pg8000",
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
+        )
+        
+        # Manually create and manage the cursor lifecycle
+        cursor = connection.cursor()
+        
+        # Log successful connection
+        logging.info("Database connection established successfully.")
+
+        return cursor, connection
+
+    except Exception as e:
+        # Log any errors encountered during the connection process
+        logging.error(f"Failed to create database connection: {e}", exc_info=True)
+        raise
+
+# Function that processes the data produced by DAG 1 and inserts it into PostgreSQL
+def insert_data_into_postgresql():
+
+    api_request_dict = make_api_request()
+
+    if not api_request_dict:
+        logging.info("No data returned from API, skipping insertion.")
+        return
+
+    try:
+        table_name = 'transactions_table'
+        # Establish connection to the table
+        cursor, connection = create_database_connection()
+
+        # Extract column names and values
+        columns = ' ,'.join([col+'_name' if col in ['first', 'last'] else col for col in api_request_dict.keys()])
+        placeholders = ', '.join(['%s'] * len(api_request_dict))
+        # Create the SQL query
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
+        values = tuple(api_request_dict.values())
+
+        # Execute the query
+        cursor.execute(query, values)
+        
+        # Commit the transaction after the query is inserted
+        connection.commit()
+        logging.info(f"Data inserted (CCN: {api_request_dict['cc_num']}) into {table_name}.")
+
+    except Exception as e:
+        logging.error(f"Error while inserting data into PostgreSQL: {str(e)}")
+    
+    finally:
+        # Check if cursor is not None before closing
+        if cursor:
+            cursor.close()
+        # Check if connection is not None before closing
+        if connection:
+            connection.close()
+
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 17),  # Starting date of the DAG
+}
+
+
+# Define the DAG
+with DAG(
+    dag_id='single_task_dag',  # Unique ID for the DAG
+    default_args=default_args,
+    schedule_interval='* * * * *',  # Schedule to run every minute
+    catchup=False,  # Disable backfilling
+) as dag:
+
+    # Define the single task
+    single_task = PythonOperator(
+        task_id='insert_api_data_into_gcloud_postgresql',  # Unique ID for the task
+        python_callable=insert_data_into_postgresql,  # Function to call
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Default arguments for the DAG
 default_args = {
